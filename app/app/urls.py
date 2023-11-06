df963421334e2ff5b7093405cdbd18619be1629f
Ian Uesseler <iuesseler@gmail.com>
2023-11-06 10:57:46 -0800

    add authentication and user update

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,7 +20,7 @@ from drf_spectacular.views import (
 
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,4 +31,6 @@ urlpatterns = [
         name='api-docs',
 
     ),
+    path('api/user/', include('user.urls')),
+
 ]
