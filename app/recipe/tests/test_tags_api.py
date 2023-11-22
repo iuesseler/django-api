e8ef6b33212956d56436709803e9662f113f275f
Ian Uesseler <iuesseler@gmail.com>
2023-11-21 16:33:08 -0800

    Add Tag model, serializer, view, url, and tests

---
+++ app/recipe/tests/test_tags_api.py
@@ -0,0 +1,58 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from core.models import Tag
+from recipe.serializers import TagSerializer
+
+TAGS_URL = reverse('recipe:tag-list')
+
+
+def create_user(email='user@example.com', password='password'):
+    return get_user_model().objects.create_user(email=email, password=password)
+
+
+class PublicTagsAPITests(TestCase):
+    """Test unauthenticated API requests"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(TAGS_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagsAPITests(TestCase):
+
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        Tag.objects.create(user=self.user, name='Red Meat')
+        Tag.objects.create(user=self.user, name='Desert')
+
+        res = self.client.get(TAGS_URL)
+
+        """tags order by reverse name"""
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limited_to_user(self):
+        """Test that tags returned are for specific authenticated user"""
+        user2 = create_user(email='otherUser@example.com', password='password')
+        Tag.objects.create(user=user2, name='Fruits')
+        tag = Tag.objects.create(user=self.user, name='Comfort Food')
+
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
+        self.assertEqual(res.data[0]['id'], tag.id)
